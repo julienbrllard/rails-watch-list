7f041d72688cdfc3dc290fb1108c7c91c79fce95
Julien Brieallard <julien.brieallard@gmail.com>
2022-05-19 17:07:38 +0200

    v

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,37 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(lists_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to root_path, status: :see_other
+  end
+end
+
+private
+
+  def lists_params
+    params.require(:list).permit(:name)
+  end
+
+  def set_lists
+    @list = List.find(params[:id])
+  end
